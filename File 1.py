d0ac2cbbf3780d376cb8fe5a57a36f7392724a2f
Kaveesha <kaveeshavimukthi688@gmail.com>
2026-01-29 13:55:55 +0530

    Initial commit : Added python script and readme

---
+++ File 1.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    """
+    Perform addition of two numbers.
+    
+    Args:
+        a: First number
+        b: Second number
+    
+    Returns:
+        Sum of a and b
+    """
+    return a + b
+
+
+
+if __name__ == "__main__":
+    result = add(5, 3)
+    print(f"5 + 3 = {result}")
---
+++ readme.md
@@ -0,0 +1,12 @@
+## `add(a, b)` Function
+
+A simple utility function that performs addition of two numbers.
+
+**Parameters:**
+- `a` (int/float): First number to be added
+- `b` (int/float): Second number to be added
+
+**Returns:**
+- (int/float): The sum of the two input numbers
+
+**Example:**
